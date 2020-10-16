d101922b28d23afb74cacbdf3b6349b7f3d49dc8
Lei Wang <wangzl@amazon.com>
2020-10-15 20:17:56 -0700

    support ai by wrapper

--- sample-apps/python-lambda/function/lambda_function.py
+++ sample-apps/python-lambda/function/lambda_function.py
@@ -3,68 +3,6 @@ import logging
 import jsonpickle
 import boto3
 
-logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all
-patch_all()
-
-from opentelemetry import trace
-# aws propagator
-from opentelemetry.propagator.xray_id_generator import AWSXRayIdsGenerator
-
-from opentelemetry.sdk.resources import (
-    Resource,
-    OTELResourceDetector,
-    get_aggregated_resources,
-)
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    ConsoleSpanExporter,
-    SimpleExportSpanProcessor,
-)
-
-from opentelemetry.resource import AwsLambdaResourceDetector
-# resource looks weird because get_aggregated_resources has bug, cannot merge _DEFAULT otel attributes
-trace.set_tracer_provider(TracerProvider(
-    ids_generator=AWSXRayIdsGenerator(), 
-    resource=Resource.create().merge(AwsLambdaResourceDetector().detect())),
-)
-# trace.set_tracer_provider(TracerProvider())
-
-
-
-# === jaeger exporter
-# from opentelemetry.exporter import jaeger
-# jaeger_exporter = jaeger.JaegerSpanExporter(
-#     service_name="my-multi-processors",
-#     agent_host_name="localhost",
-#     agent_port=6831,
-# )
-# trace.get_tracer_provider().add_span_processor(
-#     SimpleExportSpanProcessor(jaeger_exporter)
-# )
-
-trace.get_tracer_provider().add_span_processor(
-    SimpleExportSpanProcessor(ConsoleSpanExporter())
-)
-
-# === otlp exporter, collector
-# from opentelemetry.exporter.otlp.trace_exporter import OTLPSpanExporter
-# otlp_exporter = OTLPSpanExporter(endpoint="localhost:55680")
-# span_processor = SimpleExportSpanProcessor(otlp_exporter)
-# trace.get_tracer_provider().add_span_processor(span_processor)
-
-# === xray daemon exporter, xray daemon
-from opentelemetry.exporter.xraydaemon import XrayDaemonSpanExporter
-xrayDaemonSpanExporter = XrayDaemonSpanExporter()
-trace.get_tracer_provider().add_span_processor(
-    SimpleExportSpanProcessor(xrayDaemonSpanExporter)
-)
-
-tracer = trace.get_tracer(__name__)
-
 # Customer's lambda function
 def lambda_handler(event, context):
     # logger.info('## ENVIRONMENT VARIABLES\r' + jsonpickle.encode(dict(**os.environ)))
@@ -77,8 +15,3 @@ def lambda_handler(event, context):
 
     return "200 OK"
 
-# Manual enable otel instrumentation. Can remove them once we package auto-instrumentation into lambda layer.
-from opentelemetry.instrumentation.botocore import BotocoreInstrumentor
-from opentelemetry.instrumentation.awslambda import AwsLambdaInstrumentor
-BotocoreInstrumentor().instrument(tracer_provider=trace.get_tracer_provider())
-AwsLambdaInstrumentor().instrument(tracer_provider=trace.get_tracer_provider())
--- sample-apps/python-lambda/function/opentelemetry/instrumentation/awslambda/__init__.py
+++ sample-apps/python-lambda/function/opentelemetry/instrumentation/awslambda/__init__.py
@@ -81,5 +81,6 @@ class AwsLambdaInstrumentor(BaseInstrumentor):
         self.ctx_aws_request_id = lambda_context.aws_request_id
 
         # logger.info('--- env variables ---')
-        self.lambda_handler = os.environ['_HANDLER']
+        # self.lambda_handler = os.environ['_HANDLER']
+        self.lambda_handler = os.environ.get("LAMBDA_HANDLER", os.environ.get("_HANDLER"))
         self.xray_trace_id = os.environ.get('_X_AMZN_TRACE_ID', '')
