b0510ebfa262a3a809441465bbc9a7382972cf52
Lei Wang <wangzl@amazon.com>
2020-10-15 20:14:10 -0700

    support ai by wrapper

--- sample-apps/python-lambda-local/function/opentelemetry/resource/__init__.py
+++ sample-apps/python-lambda-local/function/opentelemetry/resource/__init__.py
@@ -6,13 +6,13 @@ from opentelemetry.sdk.resources import (
 
 class AwsLambdaResourceDetector(ResourceDetector):
     def detect(self) -> "Resource":
-        lambda_handler = os.environ['_HANDLER']
+        lambda_handler = os.environ.get("LAMBDA_HANDLER", os.environ.get("_HANDLER"))
         aws_region = os.environ['AWS_REGION']
         env_resource_map = {
             'cloud.region': aws_region,
             'cloud.provider': 'aws',
             'faas.name': lambda_handler,
-            # faas.id is in lambda context, hard to be extract in the beginning.
+            # faas.id is in lambda context, can to be extracted before lambda handler.
             # 'faas.id': self._ctx_invoked_function_arn,
         }
         return Resource(env_resource_map)
