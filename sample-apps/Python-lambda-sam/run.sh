66aea9ff3b151e0a8842bceef657f2cad5df9a13
Lei Wang <wangzl@amazon.com>
2020-11-08 01:00:43 -0800

    clean code

--- sample-apps/Python-lambda-sam/run.sh
+++ sample-apps/Python-lambda-sam/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+TEMPLATE=template.yml
+CONFIG=samconfig.toml
+# TEMPLATE=template-py37.yml
+# CONFIG=samconfig-py37.toml
 rm -rf .aws-sam
-sam build -u -t template-py38.yml
-find .aws-sam -name __pycache__ -exec rm -rf  {} \; > /dev/null
-sam deploy --config-file samconfig.toml
+sam build -u -t $TEMPLATE
+find .aws-sam -name __pycache__ -exec rm -rf  {} \;> /dev/null
+sam deploy --config-file $CONFIG
--- sample-apps/Python-lambda-sam/run37.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf .aws-sam
-sam build -u -t template-py37.yml
-find .aws-sam -name __pycache__ -exec rm -rf  {} \; > /dev/null
-sam deploy --config-file samconfig-py37.toml
