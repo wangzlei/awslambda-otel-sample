fa3f39c0ede57d02ac191fbea8d50b68a60f0e28
Lei Wang <66336933+wangzlei@users.noreply.github.com>
2020-11-10 23:43:55 -0800

    Update run.sh

--- sample-apps/Python-lambda-sam/run.sh
+++ sample-apps/Python-lambda-sam/run.sh
@@ -10,4 +10,5 @@ mkdir aws_observability_collector && cp -r ../../extensions/aoc-extension/* aws_
 cp /Users/wangzl/workspace/aws-ob/aws-otel-collector/build/linux/aoc_linux_x86_64 aws_observability_collector
 sam build -u -t $TEMPLATE
 find .aws-sam -name __pycache__ -exec rm -rf  {} \;
-sam deploy --config-file $CONFIG
+# sam deploy --config-file $CONFIG
+sam deploy -g
