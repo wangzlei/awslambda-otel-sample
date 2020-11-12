7ad250dc25e087efe012f94d3d9c3973fe337c10
Lei Wang <66336933+wangzlei@users.noreply.github.com>
2020-11-11 16:07:43 -0800

    Update run.sh

--- sample-apps/Python-lambda-sam/run.sh
+++ sample-apps/Python-lambda-sam/run.sh
@@ -8,7 +8,7 @@ rm -rf aws_observability_collector
 mkdir aws_observability_collector && cp -r ../../extensions/aoc-extension/* aws_observability_collector
 # remove local cp if aoc lambda is ready
 # cp /Users/wangzl/workspace/aws-ob/aws-otel-collector/build/linux/aoc_linux_x86_64 aws_observability_collector
-wget -O aws_observability_collector/collector https://github.com/open-telemetry/opentelemetry-collector-contrib/releases/download/v0.14.0/otelcontribcol_linux_amd64
+wget -O aws_observability_collector/aoc_linux_x86_64 https://github.com/open-telemetry/opentelemetry-collector-contrib/releases/download/v0.14.0/otelcontribcol_linux_amd64
 sam build -u -t $TEMPLATE
 find .aws-sam -name __pycache__ -exec rm -rf  {} \;
 # sam deploy --config-file $CONFIG
